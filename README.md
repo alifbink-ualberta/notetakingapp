c3aeeecea59593328935ca52929d19ec9f749edf
Alif Bin Khorshed Ahmed <alifbink@ualberta.ca>
2025-09-19 12:02:45 +0100

    Initial commit
    Set up main structure.

---
+++ README.md
@@ -0,0 +1,3 @@
+Note taking app reconstruction based on https://note-taking-app-front-end.netlify.app.
+
+Package focus: JSX, Typescript, Vite, React
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Sidebar/>
+      <MainNotes>
+        <NoteList/>
+        <Note/>
+      </MainNotes>
+    </>
+  )
+}
+
+export default App
