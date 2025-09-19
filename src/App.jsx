860e50f7ced75fe60849aa88a2d4033a876e1299
Alif Bin Khorshed Ahmed <alifbink@ualberta.ca>
2025-09-19 15:46:55 +0100

    Completed code for each note view.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import './App.css'
+import './components/Sidebar.tsx'
+import './components/NoteView.tsx' 
+import './components/Notelist.tsx' // Windows is case insensitive, NoteList.tsx is registered as Notelist.tsx
+
 
 function App() {
 
@@ -7,7 +11,7 @@ function App() {
       <Sidebar/>
       <MainNotes>
         <NoteList/>
-        <Note/>
+        <NoteView/>
       </MainNotes>
     </>
   )
---
+++ src/components/NoteView.tsx
@@ -0,0 +1,43 @@
+import { Note } from "./types"
+
+
+type NoteProps = {
+  note: Note | null
+  onUpdate: (updatedNote: Note) => void
+}
+
+
+function NoteView({ note, onUpdate }: NoteProps) {
+  if (!note) {
+    return <div className="note-empty">Select a note to view</div>
+  }
+
+  return (
+    <div className="note">
+      <input
+        type="text"
+        value={note.title}
+        onChange={(e) => onUpdate({ ...note, title: e.target.value })}
+      />
+      <input
+        type="text"
+        value={note.tags.join(", ")}
+        onChange={(e) => onUpdate({ ...note, 
+          tags: e.target.value.split(",").map(tag => tag.trim())})}
+      />
+      <div className="last-edited">
+        Last Edited: {note.updatedAt.toLocaleString()}
+      </div>
+
+      <textarea
+        value={note.content}
+        onChange={(e) => onUpdate({ ...note, content: e.target.value })}
+      />
+      
+    </div>
+  )
+}
+
+export default NoteView
+
+  
