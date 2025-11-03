93e1072b971ab077c95aaebb877d1836ef3ac0de
Alif Bin Khorshed Ahmed <alifbink@ualberta.ca>
2025-11-03 16:06:44 +0000

    Update Button.tsx to accept children.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,24 @@
 type ButtonProps = {
-  label: string;
-  variant?: "default" | "cancel" | "special"; // optional
+  label?: string; // optional if you want icon-only buttons too
+  variant?: "default" | "cancel" | "special";
   handleClick: () => void;
+  children?: React.ReactNode; // this enables <Button>...</Button>
 };
 
-function Button({label, variant, handleClick}: ButtonProps) {
-    return <button
-    className={variant === "cancel"? "cancel" : variant === "special" ? "special" : "default"} 
-    onClick={handleClick} >{label}
+function Button({ label, variant, handleClick, children }: ButtonProps) {
+  return (
+    <button
+      className={variant === "cancel"
+        ? "cancel"
+        : variant === "special"
+        ? "special"
+        : "default"}
+      onClick={handleClick}
+    >
+      {children}
+      {label && <span className="btn-label">{label}</span>}
     </button>
+  );
 }
 
-export default Button
+export default Button;
